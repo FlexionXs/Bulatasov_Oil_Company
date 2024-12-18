da2b239f3a72c5ad40aaeb88ee1b8ab7302891de
ilyass <tumonplay2@gmail.com>
2024-12-18 22:28:32 +0500

    упр 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res)=> {
-    res.send("Hello"); //метод get получает параметры: 1) url, 2) функцию collback с параметрами запрос и ответ
+    res.send("Hello World"); //метод get получает параметры: 1) url, 2) функцию collback с параметрами запрос и ответ
 });
 
 app.use(express.json());
