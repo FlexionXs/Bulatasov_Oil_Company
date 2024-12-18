e6c7b6c6c857eaa46d9a42cfc6c2a040338d7e6a
ilyass <tumonplay2@gmail.com>
2024-12-19 03:35:00 +0500

    авторизация яндекс

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const app = express();
 
 const {body, validationResult} = require('express-validator');
+const passport = require("passport");
+const YandexStrategy = require("passport-yandex");
+const session = require("express-session");
+
+
+const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res)=> {
-    res.send("Hello World"); //метод get получает параметры: 1) url, 2) функцию collback с параметрами запрос и ответ
+const path = require("path");
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+//Для тестирования работы сервера
+app.get("/", (req, res) => {
+  //res.send("Hello"); //метод get получает параметры: 1) url, 2) функцию collback с параметрами запрос и ответ
+  res.sendFile(path.join(publicPath, "login.html")); // Отправляем файл index.html при запросе на корневой URL
 });
+
 
 const queryRoutes = require("./routes/queryRouter"); 
 app.use("/api/query", queryRoutes);
@@ -17,6 +29,10 @@ app.use("/api/procedure", procedureRoutes);
 
 const functionRoutes = require("./routes/functionRouter");
 app.use("/api/function", functionRoutes);
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser()); // Теперь можно устанавливать и извлекать файлы cookie, используя объекты res и req соответственно.
+
 
 require("dotenv").config(); // библиотека dotenv загружает переменные среды из файла .env
 
